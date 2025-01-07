7586bb15a9989267591d7c9389865a1c2d7fbb77
thang <thangndpd10419@gmail.com>
2025-01-07 10:00:52 +0700

    lab1

---
+++ bai7/evenT.js
@@ -0,0 +1,8 @@
+function onMouseOut() {
+  let i = document.getElementsByTagName("img");
+  i[0].setAttribute("src", "anh/Hinh-anh-Itachi.jpg");
+}
+function onMouseOver() {
+  let i = document.getElementsByTagName("img");
+  i[0].setAttribute("src", "anh/sunset with guitar.jpg");
+}
